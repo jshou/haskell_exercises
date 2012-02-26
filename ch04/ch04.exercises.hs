2a1fd6baee2100a5b616a62365dfafd79aafcf31
jshou <joshuahou@gmail.com>
2012-02-26 10:43:42 -0800

    some of ch04 exercises

---
+++ ch04/ch04.exercises.hs
@@ -0,0 +1,23 @@
+import Char
+
+asInt_fold :: String -> Int
+asInt_fold s
+  | head s == '-' = -1 * asInt_fold (tail s)
+  | otherwise = foldl step 0 s where
+                step a b = a*10 + digitToInt' b where
+                    digitToInt' x = let i = digitToInt x
+                          in if i < 10
+                              then i
+                              else error "non digit character"
+
+-- type ErrorMessage = String
+-- asInt_either :: String -> Either String Int
+-- asInt_either s
+--   | head s == '-' = -1 * asInt_either (tail s)
+--   | otherwise = foldl step (Right 0) s where
+--               step (Left acc) _ = Left acc
+--               step (Right acc) b = acc*10 + digitToInt' b where
+--                     digitToInt' x = let i = digitToInt x
+--                           in if i < 10
+--                               then Right i
+--                               else Left "non digit character"
